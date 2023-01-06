bf820822e618579c941dc7d558d72bef6a9fffca
UnloadingGnat <jaavinjm123@gmail.com>
2023-01-05 21:13:33 -0500

    track

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import { NextUIProvider } from '@nextui-org/react';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     // 2. Use at the root of your app
     <NextUIProvider>
       <Component {...pageProps} />
+      <Analytics />
     </NextUIProvider>
   );
 }
